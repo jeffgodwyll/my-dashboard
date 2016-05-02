ddbd87eacf6291757938c4a6ca662d7987891d37
jeffgodwyll <jeffgodwyll@gmail.com>
2016-05-02 00:52:08 +0000

    Update sample config info

--- config_sample.py
+++ config_sample.py
@@ -2,13 +2,21 @@
 This file contains all the config values for the app.
 """
 
-# Twitter credentials
+# http://stackoverflow.com/a/7974716/2295256
+SECRET_KEY = ''
+
 TWITTER_CONSUMER_KEY = ''
 TWITTER_CONSUMER_SECRET = ''
 TWITTER_TOKEN = ''
 TWITTER_TOKEN_SECRET = ''
 
-# Lastfm credentials
+
 LASTFM_USER = ''
 LASTFM_API_KEY = ''
 LASTFM_SECRET = ''
+
+
+GOOGLE_CLIENT_ID = ''
+GOOGLE_CLIENT_SECRET = ''
+GOOGLE_REDIRECT_URI = ''
+SCOPE = ''
