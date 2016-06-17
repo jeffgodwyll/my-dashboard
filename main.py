fd720cc48aa3dcff23fe4ed5232c56f387f370bf
jeffgodwyll <jeffgodwyll@gmail.com>
2016-06-17 00:21:42 +0000

    Remove old method of getting fit data

--- main.py
+++ main.py
@@ -52,31 +52,6 @@ def http():
 http = http()
 
 
-@app.route('/oauth2callback')
-def oauth2callback():
-    if 'code' not in request.args:
-        auth_uri = (
-            'https://accounts.google.com/o/oauth2/v2/auth?response_type=code'
-            '&client_id={}&redirect_uri={}&scope={}'.format(
-                app.config['GOOGLE_CLIENT_ID'],
-                app.config['GOOGLE_REDIRECT_URI'],
-                app.config['SCOPE']))
-        return redirect(auth_uri)
-    else:
-        auth_code = request.args.get('code')
-        fields = {'code': auth_code,
-                  'client_id': app.config['GOOGLE_CLIENT_ID'],
-                  'client_secret': app.config['GOOGLE_CLIENT_SECRET'],
-                  'redirect_uri': app.config['GOOGLE_REDIRECT_URI'],
-                  'grant_type': 'authorization_code'
-                  }
-        r = http.request_encode_body(
-            'POST', 'https://www.googleapis.com/oauth2/v4/token', fields=fields,
-            encode_multipart=False)
-        session['credentials'] = r.data  # r.text?
-        return redirect(url_for('fit'))
-
-
 @app.route('/stackoverflow')
 def stackoverflow():
     req_uri = ('https://api.stackexchange.com/2.2/users/{}?order=desc&'
@@ -119,21 +94,7 @@ def fit_datasources():
     service = fit_client()
     datasources = service.users().dataSources().list(userId='me').execute()
     return datasources
-@app.route('/fit')
-def fit():
-    if 'credentials' not in session:
-        return redirect(url_for('oauth2callback'))
-    credentials = json.loads(session['credentials'])
-    if credentials['expires_in'] <= 0:
-        return redirect(url_for('oauth2callback'))
-    else:
-        headers = {
-            'Authorization': 'Bearer {}'.format(credentials['access_token'])}
-        req_uri = 'https://www.googleapis.com/fitness/v1/users/me/dataSources'
 
-        r = http.request('GET', req_uri, headers=headers)
-        resp = json.loads(r.data.decode('utf-8'))
-        return jsonify(**resp)
 
 
 def fit_datasets():
