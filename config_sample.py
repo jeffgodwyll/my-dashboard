e160dcb3202ac9bbcb90129f0b3381bc2f44cc42
jeffgodwyll <jeffgodwyll@gmail.com>
2016-06-19 02:09:33 +0000

    Update config sample

--- config_sample.py
+++ config_sample.py
@@ -2,6 +2,8 @@
 This file contains all the config values for the app.
 """
 
+ROOT_URL = 'https://<application-id>.appspot.com'
+
 # http://stackoverflow.com/a/7974716/2295256
 SECRET_KEY = ''
 
@@ -20,3 +22,33 @@ GOOGLE_CLIENT_ID = ''
 GOOGLE_CLIENT_SECRET = ''
 GOOGLE_REDIRECT_URI = ''
 SCOPE = ''
+
+
+STACKOVERFLOW_CLIENT_ID = ''
+STACKOVERFLOW_CLIENT_SECRET = ''
+STACKOVERFLOW_API_KEY = ''
+STACKOVERFLOW_REDIRECT_URI = ''
+STACKOVERFLOW_USER_ID = ''
+
+LASTFM_USER = ''
+LASTFM_API_KEY = ''
+LASTFM_SECRET = ''
+
+
+GOODREADS_KEY = ''
+GOODREADS_SECRET = ''
+GOODREADS_USERID = ''
+
+
+GOOGLE_CLIENT_ID = ''
+GOOGLE_CLIENT_SECRET = ''
+# Generate Refresh Token with:
+# https://gist.github.com/jeffgodwyll/eecea074563e7143e4a651e1a09176da
+GOOGLE_REFRESH_TOKEN = ''
+GOOGLE_FIT_SOURCE = (
+    'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps'
+)
+SCOPE = 'https://www.googleapis.com/auth/fitness.activity.read'
+
+
+HN_USER = ''
